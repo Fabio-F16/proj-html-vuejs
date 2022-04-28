1db99e95a429848d9c1e4124ebe3673dacfadbd9
Fabio-F16 <fipsfabio95@gmail.com>
2022-04-28 15:56:13 +0200

    Footer fixed

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -7,25 +7,31 @@
           <p>382 NE 191ST St # 87394 Miami, FL 33179-5574</p>
           <p>+1(305) 547-4434 (9am - 5pm, Monday - Friday)</p>
           <p>support@maxcoach.com</p>
-          <div>
+          <div class="d-flex col-6 justify-content-between py-3">
             <i class="fa-brands fa-facebook-square"></i>
             <i class="fa-brands fa-twitter"></i>
             <i class="fa-brands fa-instagram"></i>
             <i class="fa-brands fa-linkedin"></i>
           </div>
         </div>
-        <div class="col-12 col-md-6">
+        <div class="col-12 col-md-6 text-middle">
           <div
-            class="d-inline-block px-4"
+            class="d-inline-block px-4 align-top"
             v-for="(item, index) in footerItem"
             :key="index"
           >
             <h6>{{ item.title }}</h6>
             <ul>
-              <li v-for="(text, i) in item.text" :key="i">{{ text }}</li>
+              <li v-for="(text, i) in item.text" :key="i">
+                <a href="#"> {{ text }}</a>
+              </li>
             </ul>
           </div>
-          <!-- </div> -->
+        </div>
+        <div class="col-12 text-center pt-5">
+          <p>
+            <i class="fa-solid fa-copyright"></i> MaxCoach. All Rights Reserved
+          </p>
         </div>
       </div>
     </div>
@@ -36,9 +42,6 @@
 import footerItem from "@/assets/data/footerItem.json";
 export default {
   name: "FooterComponent",
-  props: {
-    // linkFooter: Array,
-  },
   data() {
     return {
       footerItem,
@@ -48,4 +51,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  li {
+    margin-top: 10px;
+  }
+}
+p,
+li,
+i,
+a {
+  font-size: 0.8rem;
+  color: #9ea4a5;
+  text-decoration: none;
+}
+i {
+  font-size: 1.5rem;
+}
+.text-center i {
+  font-size: 0.8rem;
+}
 </style>
